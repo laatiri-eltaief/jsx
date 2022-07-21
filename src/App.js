d4db862da403c8fd9038e2a270f2505f057e5937
afef <afeflatiri@gmail.com>
2022-07-21 12:03:47 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { FullName } from './component/profile/Full Name';
 import { Profilephoto } from './component/profile/Profile photo';
 import { Address } from './component/profile/Adress';
+import Navigation from './component/Navigation';
 
 function App() {
   return (
     <div className="App">
+      <Navigation/>
       <FullName/>
       <Profilephoto/>
      <Address/>
     </div>
+    
   );
 }
 
